b55003c1e99a9ffc0e59fca112d5486e15143a5c
Y.Okamoto <yukiokaka@gmail.com>
2014-03-18 12:28:49 +0900

    modified enmu function enable to read csv devicelist

--- devicedata.py
+++ devicedata.py
@@ -16,27 +16,60 @@ class Devicedata():
                 self.std_or_ext =  row[0]
                 self.devicekind_id = int(row[1], 16)
             elif row[0] == "cmd":
-                self.cmd[row[2]] = row[1]
+                self.cmd[row[2].lstrip()] = row[1].lstrip()
                 
 
 
     def printdata(self):
         print("devicename = %s, %s, devicekind = %d" %(self.devicename, self.std_or_ext, self.devicekind_id))
-        print("CMD= ", self.cmd)
+        print("==CMD List==")
+        for cmd_name, cmd_id in self.cmd.items(): 
+            print(cmd_name, ":", cmd_id)
+        print("============")
 
 
 def read_devicedata():
-    devicelist = []
+    devicelist = {}
     files = glob.glob("devicelist/*.csv")
+    
     for file in files:
         with open(file, "r") as f:
             read_data = csv.reader(f)
-            devicelist.append(Devicedata(read_data))
-    
-    for i in devicelist:
-        i.printdata()
-
+            new_device = Devicedata(read_data)
+            devicelist[new_device.devicekind_id] = new_device
+    for i, data in devicelist.items():
+        data.printdata()
+        
     return devicelist
 
+
+def encode_line(line):
+    msg_id = ""
+    data_msg = ""
+    devkind = 0
+    devindex = 0
+    data = ""
+    err_flg = 0
+    if line[0] == "R":
+        data_msg = line[1]
+        ext_id_msg = line[2]
+        if ext_id_msg == "S":
+            msg_id = int(line[3:6], 16)
+            devkind = (msg_id & 0x70) >> 4
+            devindex = msg_id & 0xF
+          
+        elif ext_id_msg == "E":
+            msg_id = int(line[3:6] + line[7:12], 16)
+            devkind = (msg_id & 0xFF00) >> 8
+            devindex = (msg_id & 0xFF)
+
+        data = line[13:]
+    else:
+        err_flg = 1
+        
+    return data_msg, devkind, devindex, data, err_flg
+
 if __name__ == "__main__":
-    read_devicedata()
+    devicelist = read_devicedata()
+    data_msg, devkind, devindex, data = encode_line("RDE001-00001:xxyyzzww")
+    print("Deivce:%s, kind = %d, index = %d" %(devicelist[devkind].devicename, devkind, devindex))
--- enum.py
+++ enum.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 from canmonitor import Canmonitor
+from devicedata import *
 import sys
 
 
-def encode_line(line):
-    print(line)
-
-
-def enum(canmonitor):
+def enum(canmonitor, devicelist = {}):
     with  canmonitor:
         canmonitor.f.write("F")
         canmonitor.f.write("WDS181-00000:")
@@ -25,14 +22,16 @@ def enum(canmonitor):
         #encode data from CANmonitor
         for f in readables:
             for line in f:
-                encode_line(line.rstrip())
+                data_msg, devkind, devindex, data, err_flg = encode_line(line)
+                if not(err_flg):
+                    print("Deivce:%s, kind = %d, index = %d" %(devicelist[devkind].devicename, devkind, devindex))
                 
                 
 
 
 if __name__ == "__main__":
     canmonitor = Canmonitor("./test.txt")
-    enum(canmonitor)
+    enum(canmonitor, read_devicedata())
 
 
 
